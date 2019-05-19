89e87188c947704f65ef098a3a1363eddef0fc71
Bola Bally <bolabally@gmail.com>
2019-05-19 12:27:36 +0000

    addindex.html

---
+++ controllers/forloop.py
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+if even numbers then 
+
+print(apple)
+
+if odd numbers 
+print(orange)
+</body>
+</html>
+
+n = int(input("2 : "))
+
+evens = []
+odds = [] 
+
+for i in range(1, 100):
+   
+    if(i%2 == 0):
+        print('apple')
+
+for i in range(n):
+    if(i%2 ! = 0):
+        print('orange')
+
---
+++ controllers/task3.py
@@ -0,0 +1,9 @@
+
+
+for i in range(1, 100):
+    if(i%2 == 0):
+        print(apple)
+
+    if(i%2 ! = 0):
+        print(orange)
+
